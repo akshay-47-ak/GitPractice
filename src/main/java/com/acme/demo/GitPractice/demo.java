6f7fc9a5103c623716d094053d7f498b984104df
akshay_a_athavale <akshayathavale1525@gmail.com>
2025-07-28 15:44:40 +0530

    Creating a conflict

--- src/main/java/com/acme/demo/GitPractice/demo.java
+++ src/main/java/com/acme/demo/GitPractice/demo.java
@@ -5,7 +5,11 @@ public class demo {
 	public static void main(String[] args) {
 		System.out.println("hello git");
 		
-		System.out.println("merge");
+		System.out.println("Happy");
+		
+		System.out.println("Conflict");
+		
+		
 	}
 	
 }
